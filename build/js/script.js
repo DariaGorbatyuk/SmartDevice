5ea70e5c053de47d9921ebeab960228ae52459cf
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-02-22 15:34:05 +0300

    Модельное окно нормально работает

--- build/js/script.js
+++ build/js/script.js
@@ -2,16 +2,15 @@
 const body = document.querySelector('body');
 const modalTemplate = document.querySelector('#modal').content.querySelector('.modal');
 const newModal = modalTemplate.cloneNode(true);
-//const closeModalBtn = newModal.querySelector('.modal__close');
 const openModalBtn = body.querySelector('.header__link-modal');
 
 function onCloseModal(evt) {
-  evt.preventDefault();
-  console.log(evt.target);
   if (evt.key !== 'Escape' && !evt.target.classList.contains('modal__close') && !evt.target.classList.contains('modal')) {
     return;
   }
+  evt.preventDefault();
   newModal.remove();
+  document.removeEventListener('keydown', onCloseModal);
 }
 
 function onOpenModal(evt) {
@@ -19,7 +18,10 @@ function onOpenModal(evt) {
   body.insertAdjacentElement('afterbegin', newModal);
   newModal.querySelector('.modal__close').addEventListener('click', onCloseModal);
   document.addEventListener('keydown', onCloseModal);
-  newModal.addEventListener('click',onCloseModal)
+  newModal.addEventListener('click', onCloseModal);
+  const name = newModal.querySelector('#name-m');
+  const tel = newModal.querySelector('#tel-m');
+  name.focus();
 }
 
 
