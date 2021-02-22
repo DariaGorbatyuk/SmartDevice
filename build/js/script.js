d339a5fef2984a34207ea4043e4bd285ff35587d
DariaGorbatyuk <dasha.gorbatyuk.97@mail.ru>
2021-02-22 23:05:57 +0300

    Добавляет нормальную отправку формы

--- build/js/script.js
+++ build/js/script.js
@@ -1,11 +1,13 @@
-'use strict';
-const body = document.querySelector('body');
-const modalTemplate = document.querySelector('#modal').content.querySelector('.modal');
+`use strict`;
+const body = document.querySelector(`body`);
+const modalTemplate = document.querySelector(`#modal`).content.querySelector(`.modal`);
 const newModal = modalTemplate.cloneNode(true);
-const openModalBtn = body.querySelector('.header__link-modal');
-const modalForm = newModal.querySelector('.modal__form');
-const questionsForm = document.querySelector('.question__form ');
-const questionsTel = questionsForm.querySelector('#tel');
+const openModalBtn = body.querySelector(`.header__link-modal`);
+const modalForm = newModal.querySelector(`.modal__form`);
+const questionsForm = document.querySelector(`.question__form `);
+const questionsTel = questionsForm.querySelector(`#tel`);
+const questionsName = questionsForm.querySelector(`#name`);
+const questionsMessage = questionsForm.querySelector(`#text`);
 
 let isStorageSupport = true;
 let storageSupportName;
@@ -13,34 +15,14 @@ let storageSupportTel;
 let storageSupportMessage;
 
 try {
-  storageSupportName = localStorage.getItem('name');
-  storageSupportTel = localStorage.getItem('tel');
-  storageSupportMessage = localStorage.getItem('message');
+  storageSupportName = localStorage.getItem(`name`);
+  storageSupportTel = localStorage.getItem(`tel`);
+  storageSupportMessage = localStorage.getItem(`message`);
 } catch (err) {
   isStorageSupport = false;
 }
 
-function onCloseModal(evt) {
-  if (evt.key !== 'Escape' && !evt.target.classList.contains('modal__close') && !evt.target.classList.contains('modal')) {
-    return;
-  }
-  evt.preventDefault();
-  newModal.remove();
-  document.removeEventListener('keydown', onCloseModal);
-}
-
-function onOpenModal(evt) {
-  evt.preventDefault();
-  body.insertAdjacentElement('afterbegin', newModal);
-  newModal.querySelector('.modal__close').addEventListener('click', onCloseModal);
-  document.addEventListener('keydown', onCloseModal);
-  newModal.addEventListener('click', onCloseModal);
-  const name = newModal.querySelector('#name-m');
-  const tel = newModal.querySelector('#tel-m');
-  const message = newModal.querySelector('#text-m');
-  name.focus();
-  tel.addEventListener('focus', onTelChange)
-  tel.addEventListener('input', onTelChange);
+function setStorage(name, tel, message) {
   if (isStorageSupport) {
     name.value = storageSupportName;
     tel.value = storageSupportTel;
@@ -48,44 +30,70 @@ function onOpenModal(evt) {
   }
 }
 
-function checkAgreement() {
-  return document.querySelector('#agree-m').checked;
-
+function onCloseModal(evt) {
+  if (evt.key !== `Escape` && !evt.target.classList.contains(`modal__close`) && !evt.target.classList.contains(`modal`)) {
+    return;
+  }
+  evt.preventDefault();
+  newModal.remove();
+  document.removeEventListener(`keydown`, onCloseModal);
 }
+
+function onOpenModal(evt) {
+  evt.preventDefault();
+  body.insertAdjacentElement(`afterbegin`, newModal);
+  newModal.querySelector(`.modal__close`).addEventListener(`click`, onCloseModal);
+  document.addEventListener(`keydown`, onCloseModal);
+  newModal.addEventListener(`click`, onCloseModal);
+  const name = newModal.querySelector(`#name-m`);
+  const tel = newModal.querySelector(`#tel-m`);
+  const message = newModal.querySelector(`#text-m`);
+  name.focus();
+  tel.addEventListener(`focus`, onTelChange)
+  tel.addEventListener(`input`, onTelChange);
+  setStorage(name, tel, message);
+}
+
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  if(!checkAgreement()){
+  if(!this.querySelector(`input[name="agree"]`).checked){
     return;
   }
   if (isStorageSupport) {
-    localStorage.setItem('name', newModal.querySelector(`#name-m`).value);
-    localStorage.setItem('tel', newModal.querySelector(`#tel-m`).value);
-    localStorage.setItem('message', newModal.querySelector(`#text-m`).value);
+    localStorage.setItem(`name`, this.querySelector(`input[name="name"]`).value);
+    localStorage.setItem(`tel`, this.querySelector(`input[name="tel"]`).value);
+    localStorage.setItem(`message`, this.querySelector(`textarea[name="text"]`).value);
   }
-  newModal.remove();
+  this.submit();
+  if(newModal){
+    newModal.remove();
+  }
 }
 
-function onTelChange(event) {
+
+function onTelChange() {
   const template = /^\+7\([0-9]{3}\)[0-9]{7}/;
-  this.addEventListener('keypress', e => {
+  this.addEventListener(`keypress`, e => {
     // Отменяем ввод не цифр
     if (!/\d/.test(e.key))
       e.preventDefault();
   });
   if (this.value.length === 0) {
-    this.value = '+7('
+    this.value = `+7(`
   } else if (this.value.length === 6) {
     this.value = `${this.value})`;
   }
   if (!template.test(this.value)) {
-    this.setCustomValidity('Пример: +7(9xx)xxxxxxx');
+    this.setCustomValidity(`Пример: +7(9xx)xxxxxxx`);
   } else {
-    this.setCustomValidity('');
+    this.setCustomValidity(``);
   }
 }
 
-openModalBtn.addEventListener('click', onOpenModal);
-modalForm.addEventListener('submit', onFormSubmit)
-questionsTel.addEventListener('input', onTelChange);
-questionsTel.addEventListener('focus', onTelChange);
+setStorage(questionsName, questionsTel, questionsMessage);
+openModalBtn.addEventListener(`click`, onOpenModal);
+modalForm.addEventListener(`submit`, onFormSubmit);
+questionsForm.addEventListener(`submit`, onFormSubmit)
+questionsTel.addEventListener(`input`, onTelChange);
+questionsTel.addEventListener(`focus`, onTelChange);
